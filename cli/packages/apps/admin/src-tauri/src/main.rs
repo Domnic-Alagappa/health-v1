3a61745d9e220e7780cb7e138d90c437563e1b65
DomnicAmalan <amalandomnic@gmail.com>
2025-11-30 21:48:18 +0530

    chore: update Rust version to 1.91 in backend and client-app Cargo.toml files; upgrade Tauri dependencies to 2.9.4 and tauri-build to 2.5.3; add new lib.rs file for admin service to encapsulate application setup logic; adjust permissions in client-app capabilities configuration for consistency

--- cli/packages/apps/admin/src-tauri/src/main.rs
+++ cli/packages/apps/admin/src-tauri/src/main.rs
@@ -2,9 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_log::Builder::new().build())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  admin_ui_lib::run();
 }
 
